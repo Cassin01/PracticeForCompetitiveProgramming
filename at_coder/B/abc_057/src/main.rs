2cdc2dad99898db8865088a9e2b0a35a73442e83
Cassin01 <koyojb@gmail.com>
2019-04-13 13:04:32 +0900

    abc_057

--- at_coder/B/abc_057/src/main.rs
+++ at_coder/B/abc_057/src/main.rs
@@ -1,3 +1,72 @@
-fn main() {
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
 
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(i64, i64); n],
+        cd: [(i64, i64); m],
+    }
+    let mut ans = vec![0; n];
+    for i in 0..ab.len() {
+        let mut min = std::i64::MAX;
+        let mut cdi = 0;
+        for j in 0..cd.len() {
+            let tmp = i64::abs(ab[i].0 - cd[j].0) + i64::abs(ab[i].1 - cd[j].1);
+            if min > tmp {
+                min = tmp;
+                cdi = j;
+            }
+        }
+        ans[i] = cdi;
+    }
+    for i in 0..ans.len() {
+        println!("{}", ans[i] + 1);
+    }
 }
